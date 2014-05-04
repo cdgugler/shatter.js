f0333118750ba39f243507d9aa5fe31844454268
Cory Gugler <cory.gugler@gmail.com>
2014-05-03 22:07:40 -0400

    Fix break from Shatter.js images property update and center sprite on stage

--- examples/js/phaser-shatter.js
+++ examples/js/phaser-shatter.js
@@ -11,21 +11,23 @@ Voronoi.Main.prototype = {
     },
 
     create: function() {
-        var earth = new Shatter(this.game.cache._images.earth.data, 4);
-        this.game.voronoiImages = earth.images;
+        var earth = new Shatter(this.game.cache._images.earth.data, 8);
         var that = this;
+        var middleX = this.game.width / 2 - this.game.cache.getImage('earth').width / 2;
+
+        this.game.voronoiImages = earth.images;
         sprites = this.game.add.group();
 
         this.game.voronoiImages.forEach(function (el, index, arr) {
             var key = 'earth' + index;
             that.game.cache.addImage(key, null, el.image);
-            var sprite = sprites.create(el.x, el.y, key);
-            sprite.body.velocity.x = that.game.rnd.integerInRange(-100, 900);
+            var sprite = sprites.create(el.x + middleX, el.y, key);
+            sprite.body.velocity.x = that.game.rnd.integerInRange(200, 900);
             sprite.body.velocity.y = that.game.rnd.integerInRange(-500, 900);
             sprite.body.setPolygon(el.points);
         });
 
-        this.game.cache.addImage('earthseg', null, this.game.voronoiImages[0][0]);
+        this.game.cache.addImage('earthseg', null, this.game.voronoiImages[0].image);
         this.game.input.maxPointers = 1;
         this.game.stage.disableVisibilityChange = true;
         this.game.physics.gravity.y = 1500;
