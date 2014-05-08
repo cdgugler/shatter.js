ce3c0b19c9904c785cfbb5da0ac42776d6a4b301
Cory Gugler <cory.gugler@gmail.com>
2014-05-07 22:47:39 -0400

    Able to add new Shatter objects to DOM example

--- examples/js/dom.js
+++ examples/js/dom.js
@@ -5,22 +5,15 @@ image.src = "img/BlueMarbleNasa.png";
 image.addEventListener("load", function() {
     var div = document.querySelectorAll('.shatter');
     var shatter = new Shatter(image, 14, 1.5);
-    // adjustment to center image on screen
-    var adjustment = (window.innerWidth / 2) - image.width / 2;
 
     // loop through images in shatter object and insert into 
     // dom at correct position
-    for (var i = 0; i < shatter.images.length; i++) {
-        shatter.images[i].image.style.position = 'absolute';
-        shatter.images[i].image.style.left = shatter.images[i].x + adjustment + 'px';
-        shatter.images[i].image.style.top = shatter.images[i].y + 75 + 'px';
-        div[0].appendChild(shatter.images[i].image);
-    }
+    placeShatter(shatter, div);
 
     // wait a bit to add dom gravity
     window.setTimeout(function() {
-        $('body').jGravity({
-            target: 'everything',
+        $('.container').jGravity({
+            target: '.shatter img',
             ignoreClass: 'ignoreMe',
             weight: 25,
             depth: 5,
@@ -28,4 +21,38 @@ image.addEventListener("load", function() {
         });
     }, 100);
 
+    // Make new shatter object
+    $('.new-shatter').on('click', function() {
+        var numPieces = $('#numPieces').val();
+        // 4 pieces if none specified
+        var shatter = new Shatter(image, numPieces || 4, 1.5);
+        placeShatter(shatter, div);
+
+
+        // clear out the input, otherwise the input box becomes unresponsive
+        $('#numPieces').val('');
+
+    // TODO
+    // figure out why it freaks out on first new shatter without a short delay before calling jGravity.
+    window.setTimeout(function() {
+        $('.container').jGravity({
+            target: '.shatter img',
+            ignoreClass: 'ignoreMe',
+            weight: 25,
+            depth: 5,
+            drag: true
+        });
+    }, 100);
+    });
 }, false);
+
+function placeShatter (shatter, domElement) {
+        // adjustment to center image on screen
+        var adjustment = (window.innerWidth / 2) - image.width / 2;
+        for (var i = 0; i < shatter.images.length; i++) {
+            shatter.images[i].image.style.position = 'absolute';
+            shatter.images[i].image.style.left = shatter.images[i].x + adjustment + 'px';
+            shatter.images[i].image.style.top = shatter.images[i].y + 75 + 'px';
+            domElement[0].appendChild(shatter.images[i].image);
+        }
+}
